987f765485a4f003647b89b2fef107271f676675
SashaBazueva <78408133+SashaBazueva@users.noreply.github.com>
2021-02-18 15:10:34 +0500

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,10 +1,10 @@
 public class Employee {
-    public String fio;
-    public String post;
-    public String email;
-    public String phoneNumber;
-    public int salary;
-    public int age;
+    private String fio;
+    private String post;
+    private String email;
+    private String phoneNumber;
+    private int salary;
+    private int age;
 
     public Employee(String fio, String post, String email, String phoneNumber, int salary, int age) {
         this.fio = fio;
@@ -15,13 +15,37 @@ public class Employee {
         this.age = age;
     }
 
-    public static void outputInformation(String fio, String post, String email, String phoneNumber, int salary, int age) {
+    public void outputInformation(String fio, String post, String email, String phoneNumber, int salary, int age) {
         System.out.println(fio);
         System.out.println();
         System.out.println("Возраст: " + age);
         System.out.println("Должность: " + post + " (" + salary + " ₽)");
         System.out.println("Эл. почта: " + email);
         System.out.println("Контактный номер телефона: " + phoneNumber);//номер телефона для связи
+    }
+
+    public int setAge() {
+        return age;
+    }
+
+    public int setSalary() {
+        return salary;
+    }
+
+    public String setFio() {
+        return fio;
+    }
+
+    public String setPost() {
+        return post;
+    }
+
+    public String setEmail() {
+        return email;
+    }
+
+    public String setPhoneNumber() {
+        return phoneNumber;
     }
 }
 
