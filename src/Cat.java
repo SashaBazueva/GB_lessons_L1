6fac37d40a0ea4c20ed1b11b9660d6831030cf1f
SashaBazueva <78408133+SashaBazueva@users.noreply.github.com>
2021-02-21 14:32:03 +0500

    Update Cat.java

--- src/Cat.java
+++ src/Cat.java
@@ -12,15 +12,15 @@ public class Cat extends Animal {
             System.out.printf("Ошибка!! --> Кот не может пробежать больше чем 200 м (Вы указали: %d м)%n", runLength);
         else if (runLength < 0)
             System.out.printf("Ошибка!! --> Проверьте введеное значение, оно не может быть отрицательным (Вы указали: %d м)%n", runLength);
-        else System.out.printf("Кот %s пробежал %d м%n", name, runLength);
+        else System.out.printf("Кот %s пробежал %d м%n", getName(), runLength);
     }
 
     @Override
     public void swim(int swimLength) {
         if (swimLength > 0)
-            System.out.printf("Ошибка!! --> Кошки не умеют плавать, поэтому кот %s не сможет проплыть %d м%n", name, swimLength);
+            System.out.printf("Ошибка!! --> Кошки не умеют плавать, поэтому кот %s не сможет проплыть %d м%n", getName(), swimLength);
         else if (swimLength < 0)
             System.out.printf("Ошибка!! --> Проверьте введеное значение, оно не может быть отрицательным (Вы указали: %d м)%n", swimLength);
-        else System.out.printf("Кошки не умеют плавать, поэтому кот %s ничего не проплыл%n", name);
+        else System.out.printf("Кошки не умеют плавать, поэтому кот %s ничего не проплыл%n", getName());
     }
 }
