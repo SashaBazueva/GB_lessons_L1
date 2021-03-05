6be62fcd1936fcaeb44c2b48e39579e171fee39b
SashaBazueva <78408133+SashaBazueva@users.noreply.github.com>
2021-03-05 17:06:08 +0500

    Изменила файл подсчета результата
    Теперь можно ставить и плюсы, и минусы

--- src/Result.java
+++ src/Result.java
@@ -8,32 +8,38 @@ public class Result {
 
     public Result(JTextField textField) {
         int index = 0;
-        s = textField.getText();
-        numbersByString = s.split(" ");
-        numbers = new int[numbersByString.length];
-        for (int i = 0; i < numbersByString.length; i++) {
-            if (!numbersByString[i].equals("+") && !numbersByString[i].equals("-")) {
-                numbers[index] = Integer.parseInt(numbersByString[i]);
-                index++;
+        if (!textField.getText().equals("")) {
+            s = textField.getText();
+            numbersByString = s.split(" ");
+            numbers = new int[numbersByString.length / 2 + 1];
+            for (int i = 0; i < numbersByString.length; i++) {
+                if (!numbersByString[i].equals("+") && !numbersByString[i].equals("-")) {
+                    numbers[index] = Integer.parseInt(numbersByString[i]);
+                    index++;
+                }
             }
+            calculateResult(numbers, numbersByString);
         }
-        calculateResult(numbers, numbersByString);
+
     }
 
     public void calculateResult(int[] numbers, String[] numbersByString) {
-        if (numbersByString[1].equals("+")) {
-            for (int i = 0; i < numbers.length; i++) {
-                result += numbers[i];
-            }
-        } else if (numbersByString[1].equals("-")) {
-            result = numbers[0];
-            for (int i = 1; i < numbers.length; i++) {
-                result -= numbers[i];
+        result = numbers[0];
+        int i = 1;
+        for (int j = 1; j < numbersByString.length + 1; j++) {
+            if (numbersByString[j - 1].equals("+")) {
+                result += numbers[i++];
+            } else if (numbersByString[j - 1].equals("-")) {
+                result -= numbers[i++];
             }
         }
     }
 
     public int getResult() {
         return result;
+    }
+
+    public void setResult(int result) {
+        this.result = result;
     }
 }
